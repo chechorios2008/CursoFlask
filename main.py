4634d9ac7b929190f9011e8644651f7debee6c07
checho rios <chechorios2008@gmail.com>
2023-03-04 19:02:38 -0500

    Archivos HTML

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
+todos = ['Hacer café', 'Montar Bici', 'Trabajar','Estudiar']
+
 @app.route('/')
-def home():
-    return "This is the home of the page with DEBUG on."
+def index():
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+    return response
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+    context = {
+        'user_ip': user_ip,
+        'todos': todos
+    }
+    return render_template('hello.HTML', **context) #con ** extando el diccionario para no escribit cada una de las variables.
