943159183deec35d652f3468d55b930a67d93d92
checho rios <chechorios2008@gmail.com>
2023-03-07 00:20:04 -0500

    Renderización de los formularios

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
-from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash, get_flashed_messages
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from flask import request, make_response, redirect, render_template, session, url_for, flash
 import unittest
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
+from app import create_app
+from app.forms import LoginForm
 
-app.config['SECRET_KEY'] = 'SUPER SECRETO'
+app = create_app()
 
-todos = ['Hacer café', 'Trabajar', 'Montar Bici','Estudiar']
+todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
 
-class LoginForm(FlaskForm):
-    username = StringField('Nombre de usuario', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
 
 @app.cli.command()
 def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner().run(tests)
 
+
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html', error=error)
+
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html', error=error)
+
 
 @app.route('/')
 def index():
@@ -32,27 +31,19 @@ def index():
 
     response = make_response(redirect('/hello'))
     session['user_ip'] = user_ip
+
     return response
 
-@app.route('/hello', methods=['GET', 'POST'])
+
+@app.route('/hello', methods=['GET'])
 def hello():
     user_ip = session.get('user_ip')
-    login_form = LoginForm()
     username = session.get('username')
 
     context = {
         'user_ip': user_ip,
         'todos': todos,
-        'login_form': login_form,
         'username': username
     }
 
-    if login_form.validate_on_submit():
-        username = login_form.username.data
-        session['username'] = username
-
-        flash('Nombre del usuario registrado con éxito.')
-
-        return redirect(url_for('index'))
-
-    return render_template('hello.HTML', **context) #con ** extando el diccionario para no escribit cada una de las variables.
+    return render_template('hello.html', **context)
